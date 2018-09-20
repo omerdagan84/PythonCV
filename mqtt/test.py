938a109fd5feb58c84b17b5de6cad75617b58986
Omer Dagan <omer.dagan@tandemg.com>
2018-09-20 15:25:41 +0300

    mqtt
    Signed-off-by: Omer Dagan <omer.dagan@tandemg.com>

---
+++ mqtt/test.py
@@ -0,0 +1,23 @@
+import time
+import paho.mqtt.client as paho
+broker="localhost"
+#define callback
+def on_message(client, userdata, message):
+    time.sleep(1)
+    print("received the test message =",str(message.payload.decode("utf-8")))
+
+client= paho.Client("client-001") #create client object client1.on_publish = on_publish #assign function to callback client1.connect(broker,port) #establish connection client1.publish("house/bulb1","on")
+######Bind function to callback
+client.on_message=on_message
+#####
+print("connecting to broker ",broker)
+client.connect(broker)#connect
+client.loop_start() #start loop to process received messages
+print("subscribing ")
+client.subscribe("house/bulb1")#subscribe
+time.sleep(2)
+print("publishing ")
+client.publish("house/bulb1","testing of mqtt")#publish
+time.sleep(4)
+client.disconnect() #disconnect
+client.loop_stop() #stop loop
